679cc312c5fc0e2fbb3e5caee5ee3de9da4ac96f
ArtForYou <kakakunyukya@gmail.com>
2023-08-30 10:38:33 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,48 @@
+package com.dicoding.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dicoding.myrecyclerview.databinding.ItemRowHeroBinding
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    class ListViewHolder(var binding: ItemRowHeroBinding) : RecyclerView.ViewHolder(binding.root) {
+//        val imagePhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+//        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+//        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemRowHeroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .into(holder.binding.imgItemPhoto)
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemDescription.text = description
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+    }
+
+    interface OnItemClickCallback{
+        fun onItemClicked(data: Hero)
+    }
+}
